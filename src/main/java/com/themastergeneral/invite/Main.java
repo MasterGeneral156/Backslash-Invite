4f605eb2f6115c7dd6e23fe94c3190fdde756adb
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-02-12 02:37:27 -0500

    Update to be optional client side

--- src/main/java/com/themastergeneral/invite/Main.java
+++ src/main/java/com/themastergeneral/invite/Main.java
@@ -12,12 +12,13 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.VERSION)
+@Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.VERSION, acceptableRemoteVersions = Main.acceptableRemoteVersion)
 public class Main 
 {
     public static final String MODID = "invite";
     public static final String MODNAME = "Backslash Invite";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
+    public static final String acceptableRemoteVersion = "*";
     
     @Instance
     public static Main instance = new Main();
